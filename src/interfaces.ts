41bb2f8222b6ab5e636aa38480105a46271a9238
Petrovich1973 <designtorg@ya.ru>
2022-06-18 20:49:35 +0300

    clear interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,25 +1,27 @@
+import React, {InputHTMLAttributes} from "react"
+
+export interface IInput extends InputHTMLAttributes<HTMLInputElement> {
+    className?: string
+    value: string | number
+    label?: string | number
+    disabled?: boolean
+    name?: string
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
 export interface ISchemeRow {
-    id: number;
-    association: string;
-    label: string;
-    handler: any;
+    id: number
+    association: string
+    label: string
+    handler: (e?: any) => any
 }
 
 export interface IDataRow {
-    r_n: number;
-    name: string;
-    age: number;
-    gender: boolean;
-    status: number;
-}
-
-export interface IInput {
-    value: string;
-    onChange: any;
-    disabled?: boolean;
-    label: any;
-    name: string;
-    className?: string;
+    r_n: number
+    name: string
+    age: number
+    gender: boolean
+    status: number
 }
 
 
